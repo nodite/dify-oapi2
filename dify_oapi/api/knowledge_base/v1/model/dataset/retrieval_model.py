48c02ec03e44f686f6f0dcb803b5941e8f5e05e1
Oscaner Miao <oscaner1997@gmail.com>
2025-07-28 15:27:55 +0800

    feat: update retrieval model structure

--- dify_oapi/api/knowledge_base/v1/model/dataset/retrieval_model.py
+++ dify_oapi/api/knowledge_base/v1/model/dataset/retrieval_model.py
@@ -6,12 +6,28 @@ from .metadata_filtering_conditions import MetadataFilteringConditions
 from .reranking_model import RerankingModel
 
 
+class KeywordSetting(BaseModel):
+    keyword_weight: float | None = None
+
+
+class VectorSetting(BaseModel):
+    vector_weight: float | None = None
+    embedding_model_name: str | None = None
+    embedding_provider_name: str | None = None
+
+
+class Weights(BaseModel):
+    keyword_setting: KeywordSetting | None = None
+
+    vector_setting: VectorSetting | None = None
+
+
 class RetrievalModel(BaseModel):
     search_method: str | None = None
     reranking_enable: bool | None = None
     reranking_mode: str | None = None
     reranking_model: RerankingModel | None = None
-    weights: float | None = None
+    weights: Weights | None = None
     top_k: int | None = None
     score_threshold_enabled: bool | None = None
     score_threshold: float | None = None

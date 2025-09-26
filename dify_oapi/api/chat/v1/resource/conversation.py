8a35ae87fca72f8ed6a349da91af40631d471cad
Charlie <charlie@castalk.com>
2025-09-26 11:25:50 +0700

    feat: export Models to __init__.py

--- dify_oapi/api/chat/v1/resource/conversation.py
+++ dify_oapi/api/chat/v1/resource/conversation.py
@@ -4,7 +4,7 @@ from dify_oapi.core.model.request_option import RequestOption
 
 from ..model.delete_conversation_request import DeleteConversationRequest
 from ..model.delete_conversation_response import DeleteConversationResponse
-from ..model.get_conversation_list_request import GetConversationsRequest
+from ..model.get_conversation_list_request import GetConversationsListRequest
 from ..model.get_conversation_list_response import GetConversationsResponse
 from ..model.get_conversation_variables_request import GetConversationVariablesRequest
 from ..model.get_conversation_variables_response import GetConversationVariablesResponse
@@ -18,10 +18,12 @@ class Conversation:
     def __init__(self, config: Config) -> None:
         self.config = config
 
-    def list(self, request: GetConversationsRequest, request_option: RequestOption) -> GetConversationsResponse:
+    def list(self, request: GetConversationsListRequest, request_option: RequestOption) -> GetConversationsResponse:
         return Transport.execute(self.config, request, unmarshal_as=GetConversationsResponse, option=request_option)
 
-    async def alist(self, request: GetConversationsRequest, request_option: RequestOption) -> GetConversationsResponse:
+    async def alist(
+        self, request: GetConversationsListRequest, request_option: RequestOption
+    ) -> GetConversationsResponse:
         return await ATransport.aexecute(
             self.config, request, unmarshal_as=GetConversationsResponse, option=request_option
         )
